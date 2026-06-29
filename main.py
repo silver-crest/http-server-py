325aa9193cc97fccf70a018a9e7856af3c6083ca
shamrocksprings10 <217861583+silver-crest@users.noreply.github.com>
2026-06-28 19:27:41 -0500

    event set to stop server thread

--- main.py
+++ main.py
@@ -3,4 +3,9 @@ from server import Server
 if __name__ == '__main__':
     server = Server(8000)
     server.loop()
+    while True:
+        line = input("Enter q to stop : ")
+        if line.strip() == "q":
+            break
+    server.stop()
 
--- server.py
+++ server.py
@@ -4,17 +4,31 @@ import threading
 from http_data import Request, Response
 
 
+def init_thread(server: socket.socket, keep_alive: threading.Event):
+    print("Thread started")
+    while not keep_alive.is_set():
+        conn, addr = server.accept()
+        t = threading.Thread(target=handle_client, args=(conn,))
+        t.start()
+    print("Thread killed")
+
 class Server:
     def __init__(self, port: int):
         self.host: str = "localhost"
         self.port: int = port
         self.socket: socket.socket = socket.create_server((self.host, self.port))
+        self.loop_thread: threading.Thread = None
+        self.keep_alive: threading.Event = threading.Event()
 
     def loop(self):
-        while True:
-            conn, addr = self.socket.accept()
-            t = threading.Thread(target=handle_client, args=(conn, ))
-            t.start()
+        print("Starting server")
+        self.keep_alive.clear()
+        self.loop_thread = threading.Thread(target=init_thread, args=(self.socket, self.keep_alive))
+        self.loop_thread.start()
+
+    def stop(self):
+        self.keep_alive.set()
+        print("Stopped server")
 
 def handle_client(conn: socket.socket):
     request = Request(conn.recv(1024).decode())
